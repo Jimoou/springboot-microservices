6aee54c086f4fbc5d816327c5a8c99cc8e366add
Jiwoon <kfromh0136@Naver.com>
2023-04-04 13:39:30 +0900

    개발환경 세팅하기
    MultiModule
    `department-service`
    `employee-service`

---
+++ department-service/src/test/java/github/Jimoou/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.Jimoou.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
