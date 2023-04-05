257fc1ff939d3c3ba2525164bd335a6821929952
Jiwoon <kfromh0136@Naver.com>
2023-04-05 14:11:11 +0900

    [#22] Feat:Resilence4j-Circuit-Breaker
    `application.properties`: 회로 차단기 관련 설정 추가
    `EmployeeServiceImpl`: @CircuitBreaker 어노테이션 및 회로 차단기가 동작할 때 사용할 대체 메서드 추가
    `build.gradle`: Resilience4j와 관련된 의존성 추가

--- employee-service/src/main/java/github/Jimoou/employeeservice/Service/implement/EmployeeServiceImpl.java
+++ employee-service/src/main/java/github/Jimoou/employeeservice/Service/implement/EmployeeServiceImpl.java
@@ -8,7 +8,7 @@ import github.Jimoou.employeeservice.Exception.ResourceNotFoundException;
 import github.Jimoou.employeeservice.Repository.EmployeeRepository;
 import github.Jimoou.employeeservice.Service.APIClient;
 import github.Jimoou.employeeservice.Service.EmployeeService;
-import lombok.AllArgsConstructor;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +30,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     return savedEmployeeDto;
   }
 
+  @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
   @Override
   public APIResponseDto getEmployeeById(Long employeeId) {
     Employee employee =
@@ -38,6 +39,25 @@ public class EmployeeServiceImpl implements EmployeeService {
             .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
 
     DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+    EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+    APIResponseDto apiResponseDto = new APIResponseDto(employeeDto, departmentDto);
+
+    return apiResponseDto;
+  }
+
+  /*fallbackMethod*/
+  public APIResponseDto getDefaultDepartment(Long employeeId, Exception e) {
+    Employee employee =
+     employeeRepository
+      .findById(employeeId)
+      .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", employeeId));
+
+    /*default Department return*/
+    DepartmentDto departmentDto = new DepartmentDto();
+    departmentDto.setDepartmentCode("RD001");
+    departmentDto.setDepartmentName("R&D Department");
+    departmentDto.setDepartmentDescription("Research and Development Department");
 
     EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
     APIResponseDto apiResponseDto = new APIResponseDto(employeeDto, departmentDto);
